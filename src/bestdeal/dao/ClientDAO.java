69c6abb69781907a6c1e2f41a9e5af79b5e9be44
ahmed <ahmed@172.16.0.58>
2014-02-27 20:42:57 +0100

    test1Git ClientAttente Class ClientDAO class ClientModel

--- src/bestdeal/dao/ClientDAO.java
+++ src/bestdeal/dao/ClientDAO.java
@@ -8,7 +8,12 @@ package bestdeal.dao;
 import bestdeal.entities.Client;
 import bestdeal.util.MyConnection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -39,6 +44,119 @@ public class ClientDAO {
             etatInscription = 0;
         }catch(SQLException e){
             etatInscription = 1;
+        }
+    }
+    
+     public List<Client> DisplayAllClients (){
+
+        List<Client> listeClients = new ArrayList<Client>();
+         //where etat_compte=1
+        String requete = "select * from client where etat_compte=1";
+        try {
+           Statement statement = MyConnection.getInstance()
+                   .createStatement();
+            ResultSet resultat = statement.executeQuery(requete);
+          //  DepotDAO depotDAO = new DepotDAO();
+            while(resultat.next()){
+                Client client =new Client();
+                client.setId_client(resultat.getInt(1));
+               client.setLogin(resultat.getString(2));
+                client.setPwd(resultat.getString(3));
+               client.setNom(resultat.getString(4));
+              client.setPrenom(resultat.getString(5));
+              client.setEmail(resultat.getString(6));
+              listeClients.add(client);
+             
+            }
+            return listeClients;
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors du chargement des clients "+ex.getMessage());
+            return null;
+        }
+    }
+     
+      public ResultSet UpdateTable()
+    {
+        try{
+            String sql ="select * from client";
+         PreparedStatement ps = MyConnection.getInstance().prepareStatement(sql);
+          ResultSet rest = ps.executeQuery(sql);
+         // tbcustomar.setModel(DbUtils.resultSetToTableModel(rest));
+          return rest;
+
+        }catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return null;
+    }
+     
+        public void deleteClient(Client c){
+
+          String requete = "delete from client where id_client=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setString(1, c.getId_client()+"");
+            ps.executeUpdate();
+            System.out.println("Suppression de client effectuée avec succès");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors de la suppression de client "+ex.getMessage());
+        }
+    }
+    
+          public void updateClient_ToInvalide(Client dl){
+        String requete = "update client set etat_compte=0 where id_client=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1, dl.getId_client());
+            ps.executeUpdate();
+            System.out.println("Client Non valide effectuée avec succès");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors de InValidation de Client "+ex.getMessage());
+        }
+    }
+             public void updateClient_ToValide(Client dl){
+        String requete = "update client set etat_compte=1 where id_client=?";
+        try {
+            PreparedStatement ps = MyConnection.getInstance().prepareStatement(requete);
+            ps.setInt(1, dl.getId_client());
+            ps.executeUpdate();
+            System.out.println("Client Valide effectuée avec succès");
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors de Validation de Client "+ex.getMessage());
+        }
+    }
+        
+        
+      public List<Client> DisplayAllClientsByName (String st){
+
+        List<Client> listeClients = new ArrayList<Client>();
+        
+        String requete = "select * from client where nom='"+st+"'" ;
+        try {
+           Statement statement = MyConnection.getInstance()
+                   .createStatement();
+            ResultSet resultat = statement.executeQuery(requete);
+          //  DepotDAO depotDAO = new DepotDAO();
+            while(resultat.next()){
+                Client client =new Client();
+                client.setId_client(resultat.getInt(1));
+               client.setLogin(resultat.getString(2));
+                client.setPwd(resultat.getString(3));
+               client.setNom(resultat.getString(4));
+              client.setPrenom(resultat.getString(5));
+              client.setEmail(resultat.getString(6));
+              listeClients.add(client);
+            }
+            return listeClients;
+        } catch (SQLException ex) {
+           //Logger.getLogger(PersonneDao.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("erreur lors du Recherche des clients "+ex.getMessage());
+            return null;
         }
     }
     
